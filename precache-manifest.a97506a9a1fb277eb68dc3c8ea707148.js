371301b39de067a0853072635ec037bc9c2a8596
Wcrafting <iam3ji0@gmail.com>
2020-01-17 10:46:41 +0300

    Updates

---
+++ precache-manifest.a97506a9a1fb277eb68dc3c8ea707148.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63bd346d420109ade32a2192b7ac282",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "c3aa469710e58e652dcc",
+    "url": "/react-weather/static/css/main.c55d15dd.chunk.css"
+  },
+  {
+    "revision": "3a66564e67f740424d89",
+    "url": "/react-weather/static/js/2.4833de1c.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/react-weather/static/js/2.4833de1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3aa469710e58e652dcc",
+    "url": "/react-weather/static/js/main.704daff5.chunk.js"
+  },
+  {
+    "revision": "a6e265e97ac9aa89d7f8",
+    "url": "/react-weather/static/js/runtime-main.d2b52519.js"
+  },
+  {
+    "revision": "29e5a64920e1829a4f1e7e51776efbdb",
+    "url": "/react-weather/static/media/cloudDay.29e5a649.jpg"
+  },
+  {
+    "revision": "4d63d4e94c9c742a8ac2c1badbbfc0ba",
+    "url": "/react-weather/static/media/day.4d63d4e9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-weather/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "46312b39980e8a13834c9cc9b7246783",
+    "url": "/react-weather/static/media/mist.46312b39.png"
+  },
+  {
+    "revision": "caaab3c4f9f3afdee5439905f18b0401",
+    "url": "/react-weather/static/media/mistDay.caaab3c4.jpg"
+  },
+  {
+    "revision": "8ce9da6fe87c87d872a2a1cdc583af34",
+    "url": "/react-weather/static/media/night.8ce9da6f.jpg"
+  },
+  {
+    "revision": "18f51a70892781a36500b05e33040484",
+    "url": "/react-weather/static/media/rainDay.18f51a70.jpg"
+  },
+  {
+    "revision": "2e70db3c5d7114c879e6769770ff0d27",
+    "url": "/react-weather/static/media/snowDay.2e70db3c.jpg"
+  },
+  {
+    "revision": "13f5b0a57bd57ec2793580d67cda926c",
+    "url": "/react-weather/static/media/sunDay.13f5b0a5.jpg"
+  }
+]);
